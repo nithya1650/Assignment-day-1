2370351c767ea5d2d46de98eb39ce098bf1919b6
nithya1650 <99589380+nithya1650@users.noreply.github.com>
2022-02-17 10:55:45 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+import numpy as np
+a=np.array([-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
+print(a)
+D = np.array(range(18)) + 3
+print(D)
+
+x=D[1:10]
+print(x)
+z=D[::-1]
+print(z)
+y=D[-17:-8]
+print(y)
